747723843e6435ab61f9d278006326497ef85c08
Rogerio Paulo <rogeriopaulos@gmail.com>
2021-11-03 16:41:50 -0300

    Adicionando celery.beat

--- fincryptos/tasks.py
+++ fincryptos/tasks.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 from fincryptos.apis.nomics import Nomics
 from fincryptos.core import send2mongo
@@ -9,6 +10,13 @@ redis_url = os.environ.get('REDIS_URL')
 
 app = Celery('fyncriptos', broker=redis_url)
 app.conf.result_backend = redis_url
+app.conf.timezone = 'America/Fortaleza'
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls every minute
+    sender.add_periodic_task(crontab(minute='*/1'), send_nomics_data2mongo.s())
 
 
 @app.task
