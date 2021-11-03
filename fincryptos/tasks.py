7bd81a32e1c79c76b3bf209e8337f97533f72247
Rogério Paulo <rogeriopaulos@gmail.com>
2021-11-03 13:34:52 -0300

    Criando a app celery da aplicação

--- fincryptos/tasks.py
+++ fincryptos/tasks.py
@@ -1,6 +1,16 @@
+import os
+
+from celery import Celery
+
+from fincryptos.apis.nomics import Nomics
 from fincryptos.core import send2mongo
-from fincryptos.cryptoapis.nomics import Nomics
+
+redis_url = os.environ.get('REDIS_URL')
+
+app = Celery('fyncriptos', broker=redis_url)
+app.conf.result_backend = redis_url
 
 
+@app.task
 def send_nomics_data2mongo():
     send2mongo(Nomics())
