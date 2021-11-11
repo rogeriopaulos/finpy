df5117056e0ff534112698feedac7484f65f84b7
Rogério Paulo <rogeriopaulos@gmail.com>
2021-11-11 13:37:19 -0300

    Parâmetro de limite no CMC (incompleto)

--- fincryptos/apis/coinmarketcap.py
+++ fincryptos/apis/coinmarketcap.py
@@ -15,33 +15,43 @@ class CoinMarketCapCryptoAPI(CryptoAPI):
     }
     collection_name = 'coinmarketcap'
 
-    def __init__(self, parameter):
-        self.parameters = parameter
+    def __init__(self):
+        self.parameters = {
+            'start': '1',
+            'limit': self.get_limit(),
+            'convert': 'USD'
+        }
 
     def __str__(self):
         return 'CoinMarketCap API'
 
-    def make_request(self) -> Response:
-        url = f'https://{self.base_url}/v1/cryptocurrency/listings/latest'
+    def get_limit(self):
+        return '5000'
+
+    def make_request(self, url_path, params=None) -> Response:
+        url = f'https://{self.base_url}/v1/cryptocurrency/{url_path}'
         session = Session()
         session.headers.update(self.headers)
-        return session.get(url, params=self.parameters)
+        if params is not None:
+            response = session.get(url, params=self.parameters)
+        else:
+            response = session.get(url)
+        return response
+
+    def get_data(self) -> Response:
+        return self.make_request('listings/latest', self.parameters)
 
 
 class CoinMarketCap(BaseAPI):
 
+    api = CoinMarketCapCryptoAPI()
+
     def get_api_data(self) -> dict:
-        parameters = {
-            'start': '1',
-            'limit': '5000',
-            'convert': 'USD'
-        }
-        api = CoinMarketCapCryptoAPI(parameters)
-        response = api.make_request()
+        response = self.api.get_data()
         data = response.json()
         return {
             'data': data['data'],
-            'source': api.__str__(),
-            'collection_name': api.collection_name,
+            'source': self.api.__str__(),
+            'collection_name': self.api.collection_name,
             'status_code': response.status_code
         }
